cda167d8d6b0ac1b4230f83bb5dfe80580b1ab29
yacaFx <sergiobritor@gmail.com>
2017-06-28 09:57:50 -0500

    Base styles has been added to my reddit app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Se agrego el link: ${link.value} con el título: ${title.value}`);
+    return false;
+  }
 }
