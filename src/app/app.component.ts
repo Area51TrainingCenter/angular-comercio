455f0540b00c9438e939b587bd5e75bf2d7bf5ab
yacaFx <sergiobritor@gmail.com>
2017-06-28 19:38:34 -0500

    Domain text and sorted functions has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
 
   constructor() {
     this.articles = [
-      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Angular 2', 'http://angular.io/components', 3),
       new Article('Stackoverflow', 'http://stackoverflow.com', 20),
       new Article('Github', 'http://github.com', 5)
     ]
@@ -22,6 +22,13 @@ export class AppComponent {
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Se agrego el link: ${link.value} con el título: ${title.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes)
   }
 }
