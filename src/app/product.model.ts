fc9326e880661012db98c2b4abc5aa08fb21c64e
yacaFx <sergiobritor@gmail.com>
2017-07-05 19:15:21 -0500

    New dist version

--- src/app/product.model.ts
+++
@@ -1,9 +0,0 @@
-export class Product {
-  constructor(
-    public sku: string,
-    public name: string,
-    public imageUrl: string,
-    public category: string[],
-    public price: number
-  ) { }
-}
