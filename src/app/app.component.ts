7aa0c67967c150893ac4e16ab0d2f18b0d525ff8
yacaFx <sergiobritor@gmail.com>
2017-07-05 20:57:03 -0500

    Inject ExampleDefs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Inject
+} from '@angular/core';
+
+import { ExampleDef } from './example.model'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor( @Inject('ExampleDefs') public examples: ExampleDef[]) { }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarItemComponent } from './sidebar/sidebar-item.component';
+import { IntroComponent } from './intro/intro.component';
+
+
+import { ExampleDef } from './example.model';
+
+
+// Master list
+
+export const examples: ExampleDef[] = [
+  { label: 'Intro', name: 'Root', path: '', component: IntroComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    SidebarItemComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule
