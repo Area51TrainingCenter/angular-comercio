b3c01953a5a3f2feef545e8481274cd9571a0c01
yacaFx <sergiobritor@gmail.com>
2017-07-10 19:36:41 -0500

    ngSwitch!

--- src/app/ng-switch-example/ng-switch-example.component.ts
+++ src/app/ng-switch-example/ng-switch-example.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ng-switch-example',
   templateUrl: './ng-switch-example.component.html'
 })
-export class NgSwitchExampleComponent {
-
+export class NgSwitchExampleComponent implements OnInit {
+  choice: number;
   constructor() { }
 
+  ngOnInit() {
+    this.choice = 1;
+  }
+
+  nextChoice(): void {
+    this.choice += 1;
+    if (this.choice > 5) {
+      this.choice = 1;
+    }
+  }
 }
