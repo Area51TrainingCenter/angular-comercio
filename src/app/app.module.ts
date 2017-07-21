9380d65299667115639d9b7b934d9e4db6ad552b
yacaFx <sergiobritor@gmail.com>
2017-07-21 18:35:46 -0500

    New files for counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,78 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import {
-  RouterModule,
-  Routes
-} from '@angular/router';
-
-import {
-  LocationStrategy,
-  HashLocationStrategy,
-  PathLocationStrategy
-} from '@angular/common';
-
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { LoginComponent } from './login/login.component';
-
-import { AUTH_PROVIDERS } from './auth.service';
-import { ProtectedComponent } from './protected/protected.component';
-import { LoggedInGuard } from './logged-in.guard';
-import { ProductsComponent } from './products/products.component';
-
-import {
-  routes as childRoutes,
-  ProductsModule
-} from './products/products.module'
-
-const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contacto', component: ContactComponent },
-  { path: 'contactanos', redirectTo: 'contacto' },
-  { path: 'login', component: LoginComponent },
-  {
-    path: 'protected',
-    component: ProtectedComponent,
-    canActivate: [LoggedInGuard]
-  },
-  {
-    path: 'products',
-    component: ProductsComponent,
-    children: childRoutes
-  },
-  { path: '**', component: NotFoundComponent }
-];
-
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent,
-    NotFoundComponent,
-    LoginComponent,
-    ProtectedComponent,
-    ProductsComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(
-      routes
-      //  { enableTracing: true }
-    ),
-    ProductsModule
+    BrowserModule
   ],
-  providers: [
-    AUTH_PROVIDERS,
-    LoggedInGuard,
-    { provide: LocationStrategy, useClass: PathLocationStrategy }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
