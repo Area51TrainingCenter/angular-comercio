1703166b25dc07f4cf0627d337b320af5b44da1a
yacaFx <sergiobritor@gmail.com>
2017-07-21 18:51:16 -0500

    Add actions

--- src/app/counter.actions.ts
+++ src/app/counter.actions.ts
@@ -3,3 +3,12 @@ import {
   ActionCreator
 } from 'redux';
 
+export const INCREMENT: string = 'INCREMENT';
+export const increment: ActionCreator<Action> = () => ({
+  type: INCREMENT
+})
+
+export const DECREMENT: string = 'DECREMENT';
+export const decrement: ActionCreator<Action> = () => ({
+  type: DECREMENT
+})
