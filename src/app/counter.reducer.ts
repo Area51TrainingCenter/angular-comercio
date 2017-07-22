b89d9d75bc1ec000163cabc463dd8030c5f36bb0
yacaFx <sergiobritor@gmail.com>
2017-07-21 19:01:29 -0500

    reducer

---
+++ src/app/counter.reducer.ts
@@ -0,0 +1,28 @@
+import {
+  Reducer,
+  Action
+} from 'redux';
+
+import { AppState } from './app.state';
+
+import {
+  INCREMENT,
+  DECREMENT
+} from './counter.actions';
+
+const initialState: AppState = { counter: 0 };
+
+export const counterReducer: Reducer<AppState> =
+  (state: AppState = initialState, action: Action): AppState => {
+    switch (action.type) {
+      case INCREMENT:
+        return Object.assign({}, state, { counter: state.counter + 1 });
+        break;
+      case DECREMENT:
+        return Object.assign({}, state, { counter: state.counter - 1 });
+        break;
+
+      default:
+        return state;
+    }
+  }
