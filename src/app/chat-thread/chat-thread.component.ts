8076155c541e28798a987b3ed43a635e48e4ba9d
yacaFx <sergiobritor@gmail.com>
2017-07-24 19:58:45 -0500

    chat thread

---
+++ src/app/chat-thread/chat-thread.component.ts
@@ -0,0 +1,31 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
+
+import { Thread } from '../thread/thread.model';
+
+@Component({
+  selector: 'app-chat-thread',
+  templateUrl: './chat-thread.component.html',
+  styleUrls: ['./chat-thread.component.css']
+})
+export class ChatThreadComponent implements OnInit {
+  @Input() thread: Thread;
+  @Input() selected: boolean;
+  @Output() onThreadSelected: EventEmitter<Thread>;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  clicked(event: any): void {
+    this.onThreadSelected.emit(this.thread);
+    event.preventdefault();
+  }
+
+}
