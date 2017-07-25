f72d5455bc4b2184faebcbad51f096d6d26a5a82
yacaFx <sergiobritor@gmail.com>
2017-07-24 21:48:04 -0500

    data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import * as Redux from 'redux';
 import { AppStore } from './app.store';
 import { AppState } from './app.reducer';
 
+import { ChatExampleData } from './data/chat-data';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
@@ -11,6 +13,7 @@ import { AppState } from './app.reducer';
 export class AppComponent {
   constructor( @Inject(AppStore) private store: Redux.Store<AppState>) {
     // Inyectar datos al chat
+    ChatExampleData(store);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -23,6 +24,7 @@ import { ChatPageComponent } from './chat-page/chat-page.component';
 @NgModule({
   declarations: [
     AppComponent,
+    FormsModule,
     ChatMessageComponent,
     FromNowPipe,
     ChatThreadComponent,
